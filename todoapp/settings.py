2101f5da78c9844f76e02a099c97f9796bae46fd
BlackPrince163 <niazhadik3@mail.ru>
2022-10-29 11:58:03 +0300

    + html page and link css

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = "static"
+
