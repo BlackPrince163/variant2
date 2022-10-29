e8058bc00d8cfc1469bad103297cb8301c3e3592
BlackPrince163 <niazhadik3@mail.ru>
2022-10-29 12:05:02 +0300

    ! fix bug

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,7 @@ class ToDoListSerializer(serializers.ModelSerializer):
         read_only_fields = ("done", "created_at")
 
     def create(self, validated_data):
-        done = True if validated_data.get("until_date") <= datetime.now().date() else False
+        done = validated_data.get("until_date") <= datetime.now().date()
         to_do_model = ToDoList(to_do=validated_data.get("to_do"), until_date=validated_data.get("until_date"), done=done)
         to_do_model.save()
         return to_do_model
