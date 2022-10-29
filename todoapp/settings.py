dd0b049c9595860be7b9304de8006f546d01ec49
BlackPrince163 <niazhadik3@mail.ru>
2022-10-29 10:56:50 +0300

    + init app api

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todoapp",
+        "USER": "todoapp",
+        "PASSWORD": "todoapp",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
